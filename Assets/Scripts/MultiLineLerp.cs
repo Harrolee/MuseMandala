1688756dc1658896054254b01f396b093d2de56c
Lee <lharrold@scu.edu>
2019-06-12 01:37:08 -0700

    June 11th Demo. Look in SampleScene.

--- Assets/Scripts/MultiLineLerp.cs
+++ Assets/Scripts/MultiLineLerp.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//June 11th Single Line Generation.
-
-
-
-public class LerpScript : MonoBehaviour
+public class MultiLineLerp : MonoBehaviour
 {
     // Transforms to act as start and end markers for the journey.
     Vector3 lerpStart;
+    Vector3 RlerpStart;
+    Vector3 LlerpStart;
+    Vector3 DlerpStart;
+    Vector3 UlerpStart;
+    //----------------------make these arrays, Lee. Please.
     Vector3 lerpEnd;
+    Vector3 RlerpEnd;
+    Vector3 LlerpEnd;
+    Vector3 DlerpEnd;
+    Vector3 UlerpEnd;
+
     int counter = 0;
     bool firstRun = true;
 
@@ -23,6 +29,10 @@ public class LerpScript : MonoBehaviour
     List<Vector3> pointsDown = new List<Vector3>();
     List<Vector3> pointsUp = new List<Vector3>();
     Vector3 newPoint;
+    Vector3 newPointR;
+    Vector3 newPointL;
+    Vector3 newPointD;
+    Vector3 newPointU;
     // Movement speed in units/sec.
     public float speed = 1.0F;
 
@@ -58,6 +68,15 @@ public class LerpScript : MonoBehaviour
         }
         print("Generated Points");
 
+        //mirror generated points
+        foreach (Vector3 point in points)
+        {
+            pointsRight.Add(Vector3.Reflect(point, Vector3.right));
+            pointsLeft.Add(Vector3.Reflect(point, Vector3.left));
+            pointsDown.Add(Vector3.Reflect(point, Vector3.down));
+            pointsUp.Add(Vector3.Reflect(point, Vector3.up));
+        }
+
         ResetPointLerp();
     }
     //perform inverse t
@@ -66,6 +85,7 @@ public class LerpScript : MonoBehaviour
 
     private void Update()
     {
+        //first case//////////////////////////////////////////////////////=============================
         //below is the "lerp engine"
         if (goodToGo)
         {
@@ -76,12 +96,23 @@ public class LerpScript : MonoBehaviour
             fracJourney = distCovered / journeyLength;
             // Set our position as a fraction of the distance between the markers.
 
+
+            //add 4 cases of lerpStart and lerpEnd in this check. Dist Covered and fracJourney will be the same for each case. They are reflections.
             if (distCovered < journeyLength)
             {
                 newPoint = Vector3.Lerp(lerpStart, lerpEnd, fracJourney);
+                newPointR = Vector3.Lerp(RlerpStart, RlerpEnd, fracJourney);
+                newPointL = Vector3.Lerp(LlerpStart, LlerpEnd, fracJourney);
+                newPointD = Vector3.Lerp(DlerpStart, DlerpEnd, fracJourney);
+                newPointU = Vector3.Lerp(UlerpStart, UlerpEnd, fracJourney);
 
                 //Generate the next point in the line
                 Instantiate(_CurvePoint, newPoint, Quaternion.identity, transform);
+                Instantiate(_CurvePoint, newPointR, Quaternion.identity, transform);
+                Instantiate(_CurvePoint, newPointL, Quaternion.identity, transform);
+                Instantiate(_CurvePoint, newPointD, Quaternion.identity, transform);
+                Instantiate(_CurvePoint, newPointU, Quaternion.identity, transform);
+
             }
             else //the lerp engine finishes here.
             {
@@ -90,8 +121,9 @@ public class LerpScript : MonoBehaviour
                 ResetPointLerp();
             }
         }
+        //end first case//////////////////////////////////////////////////////=============================
         else
-        print("looks like update doesn't wait for completion of logic to run again.");
+            print("looks like update doesn't wait for completion of logic to run again.");
 
         print("should be more than other print");
     }
@@ -100,10 +132,21 @@ public class LerpScript : MonoBehaviour
     //call this function whenever we begin lerping between a new set of points.
     void ResetPointLerp()
     {
+        //set all 5 reflections.
         if (firstRun)
         {
             lerpStart = start;
+            RlerpStart = start;
+            LlerpStart = start;
+            DlerpStart = start;
+            UlerpStart = start;
+
             lerpEnd = points[counter];
+            RlerpEnd = pointsLeft[counter];
+            LlerpEnd = pointsDown[counter];
+            DlerpEnd = pointsUp[counter];
+            UlerpEnd = pointsRight[counter];
+
             // Calculate the journey length.
             journeyLength = Vector3.Distance(start, lerpEnd);
             firstRun = false;
@@ -111,7 +154,17 @@ public class LerpScript : MonoBehaviour
         else
         {
             lerpStart = points[counter - 1];
+            RlerpStart = pointsLeft[counter - 1];
+            LlerpStart = pointsDown[counter - 1];
+            DlerpStart = pointsUp[counter - 1];
+            UlerpStart = pointsRight[counter - 1];
+
             lerpEnd = points[counter];
+            RlerpEnd = pointsLeft[counter];
+            LlerpEnd = pointsDown[counter];
+            DlerpEnd = pointsUp[counter];
+            UlerpEnd = pointsRight[counter];
+
             journeyLength = Vector3.Distance(lerpStart, lerpEnd);
         }
         counter++;
--- Assets/Scripts/SetPosLerp.cs
+++ Assets/Scripts/SetPosLerp.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//June 11th Single Line Generation.
-
-
-
-public class LerpScript : MonoBehaviour
+public class SetPosLerp : MonoBehaviour
 {
     // Transforms to act as start and end markers for the journey.
     Vector3 lerpStart;
+    Vector3 RlerpStart;
+    Vector3 LlerpStart;
+    Vector3 DlerpStart;
+    Vector3 UlerpStart;
+    //----------------------make these arrays, Lee. Please.
     Vector3 lerpEnd;
+    Vector3 RlerpEnd;
+    Vector3 LlerpEnd;
+    Vector3 DlerpEnd;
+    Vector3 UlerpEnd;
+
+    int numOfPoints = 10;
     int counter = 0;
     bool firstRun = true;
 
     Vector3 start;
-    public GameObject _CurvePoint;
+    public GameObject _RightLine;
 
     List<Vector3> points = new List<Vector3>();
     List<Vector3> pointsRight = new List<Vector3>();
@@ -23,6 +30,10 @@ public class LerpScript : MonoBehaviour
     List<Vector3> pointsDown = new List<Vector3>();
     List<Vector3> pointsUp = new List<Vector3>();
     Vector3 newPoint;
+    Vector3 newPointR;
+    Vector3 newPointL;
+    Vector3 newPointD;
+    Vector3 newPointU;
     // Movement speed in units/sec.
     public float speed = 1.0F;
 
@@ -32,31 +43,46 @@ public class LerpScript : MonoBehaviour
     // Total distance between the markers.
     private float journeyLength;
 
+    LineRenderer line1;
+    LineRenderer line2;
+
     bool goodToGo;
     void Start()
     {
+        line1 = GetComponent<LineRenderer>();
+        line2 = _RightLine.GetComponent<LineRenderer>();
+
         //start being the last child of the lineRenderer
-        start = transform.GetChild(transform.childCount-1).position;
+        start = transform.position;
         print("32");
         // Keep a note of the time the movement started.
         startTime = Time.time;
 
         //Generate a list of points from a simple algorithm
         points.Add(start);
-        for (int i = 1; i < 10; i++)
+        for (int i = 1; i < numOfPoints; i++)
         {
             if (i % 7 == 0)
-                points.Add(points[i-1] + new Vector3(2, 1, -3));
-            else if(i % 5 == 0)
+                points.Add(points[i - 1] + new Vector3(2, 1, -3));
+            else if (i % 5 == 0)
                 points.Add(points[i - 1] + new Vector3(2, 2, -.5f));
             else if (i % 3 == 0)
                 points.Add(points[i - 1] + new Vector3(1, 1, -1.5f));
             else if (i % 2 == 0)
                 points.Add(points[i - 1] + new Vector3(1, 0));
             else
-                points.Add(points[i-1] + new Vector3(1, 0));
+                points.Add(points[i - 1] + new Vector3(1, 0));
         }
         print("Generated Points");
+
+        //mirror generated points
+        foreach (Vector3 point in points)
+        {
+            pointsRight.Add(Vector3.Reflect(point, Vector3.right));
+            pointsLeft.Add(Vector3.Reflect(point, Vector3.left));
+            pointsDown.Add(Vector3.Reflect(point, Vector3.down));
+            pointsUp.Add(Vector3.Reflect(point, Vector3.up));
+        }
 
         ResetPointLerp();
     }
@@ -76,12 +102,18 @@ public class LerpScript : MonoBehaviour
             fracJourney = distCovered / journeyLength;
             // Set our position as a fraction of the distance between the markers.
 
+
+            //add 4 cases of lerpStart and lerpEnd in this check. Dist Covered and fracJourney will be the same for each case. They are reflections.
             if (distCovered < journeyLength)
             {
                 newPoint = Vector3.Lerp(lerpStart, lerpEnd, fracJourney);
+                newPointR = Vector3.Lerp(RlerpStart, RlerpEnd, fracJourney);
 
-                //Generate the next point in the line
-                Instantiate(_CurvePoint, newPoint, Quaternion.identity, transform);
+                line1.positionCount = counter;
+                line2.positionCount = counter;
+
+                line1.SetPosition(counter-1, newPoint);
+                line2.SetPosition(counter-1, newPointR);
             }
             else //the lerp engine finishes here.
             {
@@ -91,7 +123,7 @@ public class LerpScript : MonoBehaviour
             }
         }
         else
-        print("looks like update doesn't wait for completion of logic to run again.");
+            print("looks like update doesn't wait for completion of logic to run again.");
 
         print("should be more than other print");
     }
@@ -100,10 +132,17 @@ public class LerpScript : MonoBehaviour
     //call this function whenever we begin lerping between a new set of points.
     void ResetPointLerp()
     {
+        //set all 5 reflections.
         if (firstRun)
         {
             lerpStart = start;
+            RlerpStart = start;
+
+
             lerpEnd = points[counter];
+            RlerpEnd = pointsLeft[counter];
+
+
             // Calculate the journey length.
             journeyLength = Vector3.Distance(start, lerpEnd);
             firstRun = false;
@@ -111,7 +150,13 @@ public class LerpScript : MonoBehaviour
         else
         {
             lerpStart = points[counter - 1];
+            RlerpStart = pointsLeft[counter - 1];
+
+
             lerpEnd = points[counter];
+            RlerpEnd = pointsLeft[counter];
+
+
             journeyLength = Vector3.Distance(lerpStart, lerpEnd);
         }
         counter++;
@@ -121,34 +166,34 @@ public class LerpScript : MonoBehaviour
 
 
 
- float distCovered =0;
- float fracJourney;
- /*
-    // Follows the target position like with a spring
-    void Update()
-    {            
-        // Distance moved = time * speed.
-        distCovered = (Time.time - startTime) * speed;
+    float distCovered = 0;
+    float fracJourney;
+    /*
+       // Follows the target position like with a spring
+       void Update()
+       {            
+           // Distance moved = time * speed.
+           distCovered = (Time.time - startTime) * speed;
 
-        // Fraction of journey completed = current distance divided by total distance.
-        fracJourney = distCovered / journeyLength;
-        // Set our position as a fraction of the distance between the markers.
+           // Fraction of journey completed = current distance divided by total distance.
+           fracJourney = distCovered / journeyLength;
+           // Set our position as a fraction of the distance between the markers.
 
-        if(distCovered<journeyLength)
-        {
-            Debug.Log("added point");
-        newPoint = Vector3.Lerp(start, endMarker.position, fracJourney);
+           if(distCovered<journeyLength)
+           {
+               Debug.Log("added point");
+           newPoint = Vector3.Lerp(start, endMarker.position, fracJourney);
 
-            //check density of point generation
-        points.Add(newPoint);
+               //check density of point generation
+           points.Add(newPoint);
 
-        //Generate the line
-        Instantiate( _CurvePoint, newPoint, Quaternion.identity, transform);
-        }
-        else
-        {
-            Debug.Log("Point count is " + points.Count);
-        }
-    }
-    */
+           //Generate the line
+           Instantiate( _CurvePoint, newPoint, Quaternion.identity, transform);
+           }
+           else
+           {
+               Debug.Log("Point count is " + points.Count);
+           }
+       }
+       */
 }
