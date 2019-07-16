022bfd3e2e357f891d521c4dd2b26b1d0f1a8de4
Harrolee <Harrolee@users.noreply.github.com>
2019-07-16 09:56:56 -0700

    coroutine confusion

--- Assets/Scripts/ChildLineRunner.cs
+++ Assets/Scripts/ChildLineRunner.cs
@@ -51,23 +51,27 @@ public class ChildLineRunner : MonoBehaviour//LineSource
     //Below is Main Controlling Method.
 
     //Main Logic Method:
-    public void StartLineRender(Vector3[] myLine)
+    public void StartLineRender(Vector3[] myGivenLine)
     {
 
         //must get vars after LineSource is finished generating the section of the mandala.
         GetVarsFromParent();
         
         //why is this here? Because this is where Trunks get their lines from root.
-        this.myLine = myLine;
-   
+        myLine = myGivenLine; 
+        //At this point, each trunk has a distinct line.
+
         //spawn as many children as there will be branches.
-        CreateBranchGenerators();
+        //CreateBranchGenerators();
         
         //to test the look of the mandala, we're using PlotPoints() to plot each child's line at the same time.
-        PlotPoints();
+        //PlotPoints();
+
+        //at this point, We have all the declared point.
+        //The branches have their individual lines.
 
         //below lies slerper
-        //SendPointsToLR();
+        SendPointsToLR(0);
             //is wonky
 
         //render point array.
@@ -152,7 +156,7 @@ public class ChildLineRunner : MonoBehaviour//LineSource
 
 
 
-    //plots all the points in myLine at the same time.
+    //plots all (Declared) points in myLine at the same time.
     void PlotPoints()
     {
         int branchCue = 0;
@@ -171,6 +175,9 @@ public class ChildLineRunner : MonoBehaviour//LineSource
             lineCounter++;
         }
     }
+
+    //for each point in myLine,
+        //send 
 
     void CueBranchLR(int branch)
     {
@@ -194,59 +201,139 @@ public class ChildLineRunner : MonoBehaviour//LineSource
 
 
 
+    //Here is: a system that sends the current point and the next point into StartCoroutine(SlerpLine(______))
 
     int counter = 0;
-    //here is--> a system that sends the current point and the next point into StartCoroutine(SlerpLine(______))
+    //here is--> A versions of SendPointsToLR that discrimiates by type and time. 
 
-    void SendPointsToLR()
+    //void SendPointsToLR()
+    //{
+    //    if (counter == numPointsPerLine - 1)
+    //    {
+    //        print("done rendering line");
+    //        //could begin next section.
+    //    }
+    //    else
+    //    {
+    //        //StartCoroutine(SlerpLine(Time.time, myLine[counter], myLine[counter + 1]));
+    //        //check for branchPoints. When one is discovered, render the branch associated with that point.
+    //        if (myLine[counter] == branchCues[branchCounter])
+    //        {
+    //            RenderBranch();
+    //            branchCounter++;
+    //        }
+    //        counter++;
+    //        print("counter " + counter);
+    //    }
+    //}
+
+    int currPoint = 0;
+    void SendPointsToLR(int plottedPointCount)//Indiscriminate of order Version.
     {
-        //if (counter == numPointsPerLine -1)
+        print("myVal: " + myLine[4]);//learn if myLine stays distinct here.
+        //line is distinct at first call.
+        //every succeeding call has the same value.
+
+        //SendPointsToLR should only happen once for every point in myLine.
+        if (currPoint<myLine.Length)
+        {
+            currPoint++;
+            StartCoroutine(SlerpLine(Time.time, myLine[currPoint-1], myLine[currPoint], plottedPointCount));
+        }
+
+        //start ~15 coroutines at the same time.
+            //given that this is one of four identical nodes, at when this starts, ~60 total begin.
+       // for (int point = 1; point < myLine.Length; point++)
+        //{                                          //currpoint begins at (0,0,0), taken from the curr location of this GO
+
+
+            
+        //after rendering a line between two points, simplify it. Test whether this removes curves.
+        //myLR.Simplify(.1f);
+        //}
+        //StartCoroutine(SlerpLine(Time.time, myLine[counter], myLine[counter + 1]));
+
+
+
+        //if (counter == numPointsPerLine - 1)
         //{
         //    print("done rendering line");
-        //    //could begin next section.
+        //    could begin next section.
         //}
         //else
         //{
-        //    StartCoroutine(SlerpLine(Time.time, myLine[counter], myLine[counter + 1], speed));
-        //    //check for branchPoints. When one is discovered, render the branch associated with that point.
-        //    if(myLine[counter] == branchCues[branchCounter])
-        //    {
-        //        RenderBranch();
-        //        branchCounter++;
-        //    }
+        //    StartCoroutine(SlerpLine(Time.time, myLine[counter], myLine[counter + 1]));
+        //    check for branchPoints.When one is discovered, render the branch associated with that point.
+        //    if (myLine[counter] == branchCues[branchCounter])
+        //        {
+        //            RenderBranch();
+        //            branchCounter++;
+        //        }
         //    counter++;
         //    print("counter " + counter);
         //}
     }
 
     Vector3 increment;
-    //spherically interpolates a line between the current point and the next point.
-    //pass Time.time into this param
-    IEnumerator SlerpLine(float startTime, Vector3 currPoint, Vector3 declaredPoint, float speed)
+    ////spherically interpolates a line between the current point and the next point.
+    ////pass Time.time into this param
+    //IEnumerator SlerpLineOverTime(float startTime, Vector3 currPoint, Vector3 declaredPoint, float speed)
+    //{
+    //    float distance = Vector3.Distance(currPoint, declaredPoint);
+    //    float distanceCovered = (Time.time - startTime) * speed;
+    //    float fracJourney = distanceCovered / distance;
+    //    print("and");
+    //    while (fracJourney < 1)
+    //    {
+    //        distanceCovered = (Time.time - startTime) * speed;
+    //        fracJourney = distanceCovered / distance;
+    //        print("how far? " + fracJourney);
+
+    //        //rad. connect this to a line renderer.
+    //        increment = Vector3.Slerp(currPoint, declaredPoint, fracJourney);
+    //        myLR.positionCount = lineCounter + 1;
+    //        myLR.SetPosition(lineCounter, increment);
+    //        lineCounter++;
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //    //after the line is rendered, say that you are done.
+    //    //this message is needed to spark the next point.
+    //    print("should happen 15 times per reflection. Counter val is: " + counter);
+    //    SendPointsToLR();
+    //}
+    ////everyone is getting the same set of points.
+
+
+    ////spherically interpolates a line between the current point and the next point.
+    IEnumerator SlerpLine(float startTime, Vector3 currPoint, Vector3 declaredPoint, int plottedPointCount)
     {
+        print(currPoint + "<currPoint && declaredPoint>" + declaredPoint);
         float distance = Vector3.Distance(currPoint, declaredPoint);
-        float distanceCovered = (Time.time - startTime) * speed;
-        float fracJourney = distanceCovered / distance;
-        print("and");
+        float distanceCovered;
+        float fracJourney = 0;
+
         while (fracJourney < 1)
         {
-            distanceCovered = (Time.time - startTime) * speed;
+            distanceCovered = (Time.time - startTime);
             fracJourney = distanceCovered / distance;
             print("how far? " + fracJourney);
-
-            //rad. connect this to a line renderer.
             increment = Vector3.Slerp(currPoint, declaredPoint, fracJourney);
-            myLR.positionCount = lineCounter + 1;
-            myLR.SetPosition(lineCounter, increment);
-            lineCounter++;
-            yield return new WaitForEndOfFrame();
+            //rad. connect this to a line renderer.
+
+            myLR.positionCount = plottedPointCount + 1; //might be off by 1 point. Fix by initially incrementing plottedPointCount by 1.
+            myLR.SetPosition(plottedPointCount, increment);
+            plottedPointCount++;
+           yield return new WaitForEndOfFrame();
         }
         //after the line is rendered, say that you are done.
         //this message is needed to spark the next point.
-        print("should happen 15 times per reflection. Counter val is: " + counter);
-        SendPointsToLR();
+        //print("should happen 15 times per reflection. Counter val is: " + counter);
+        print("ought to occur 15 times and increment by 60 each time: " + plottedPointCount);
+        SendPointsToLR(plottedPointCount);
+        //counter++;
     }
     //everyone is getting the same set of points.
+
 
     void RenderBranch()
     {
