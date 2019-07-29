1e635e8a5ef21fb5864dbc624a7b0a9e4a6c603f
Harrolee <Harrolee@users.noreply.github.com>
2019-07-29 09:59:22 -0700

    Added OSCext. See Scenes/Feedback for example use

--- Assets/Scripts/GrainCTL.cs
+++ Assets/Scripts/GrainCTL.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using extOSC;
 
 public class GrainCTL : MonoBehaviour
 {
     [Range(1,5)]
     public int speed = 1;
     PostProcessVolume vol;
+    public OSCReceiver reciever;
 
     void Start()
     {
+        //osc recieve setup:
+        reciever.Bind("/muse/test", GrainOSC);
+        print("bound");
+
         vol = GetComponent<PostProcessVolume>();
     }
 
@@ -23,6 +29,43 @@ public class GrainCTL : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             vol.weight -= .025f * speed;
+        }
+    }
+
+    public void GrainOSC(OSCMessage message)
+    {
+        if(message.ToFloat(out float value))
+        {
+            print("recieved Val: " + value);
+            print("val as string" + value.ToString());
+            StartCoroutine(IncGrain(value));
+        }
+
+        //vol.weight += message;//.025f * speed;
+    }
+
+    IEnumerator IncGrain(float endVal)
+    {
+        float startWeight = vol.weight;
+        float degree = 0;
+        print("coro" + endVal);
+        if (vol.weight < endVal)
+        {
+            while (vol.weight < endVal)
+            {
+                degree += (speed * Time.deltaTime);
+                vol.weight = Mathf.Lerp(0, endVal, degree);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        else
+        {
+            while (vol.weight > endVal)
+            {
+                degree += (speed * Time.deltaTime);
+                vol.weight = Mathf.Lerp(startWeight, endVal, degree);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
