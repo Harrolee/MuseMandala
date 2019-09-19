448b43375049e856d1cfc7664ee6410c8f6123d2
Harrolee <Harrolee@users.noreply.github.com>
2019-09-19 12:04:46 -0700

    sand effect in LeeShaders

---
+++ Assets/BlowSand.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mandala;
+public class BlowSand : MonoBehaviour
+{
+    Material[] child_mats = new Material[2];
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            for (int i = 0; i < child_mats.Length; i++)
+            {
+                child_mats[i] = transform.GetChild(i).GetComponent<MeshRenderer>().material;
+            }
+            Sequence();
+        }
+    }
+
+    void Sequence()
+    {
+        float fadeTime = 4;
+        float brushTime = 1f;
+        StartCoroutine(Disappear_Sand(fadeTime, brushTime));
+    }
+
+    IEnumerator Disappear_Sand(float fadeTime, float brushTime)
+    {
+        for (int i = 0; i < child_mats.Length; i++)
+        {
+            StartCoroutine(Effects.LerpMatOverTime(child_mats[i], "_ACT", 0, 1, fadeTime));
+        }
+
+        yield return new WaitForSeconds(fadeTime);
+
+        for (int i = 0; i < child_mats.Length; i++)
+        {
+            StartCoroutine(Effects.LerpMatOverTime(child_mats[i], "_BlowAway", 0, 1, brushTime));
+        }
+
+        yield return new WaitForSeconds(brushTime);
+        for (int i = 0; i < child_mats.Length; i++)
+        {
+            child_mats[i].SetFloat("_Alpha", 0);
+        }
+    }
+}
